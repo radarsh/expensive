90c4ddbeb0ef9c8d191b9706aca68bf677894003
Adarsh Ramamurthy <adarsh.ramamurthy@gmail.com>
2011-07-31 19:45:31 +0100

    First commit.

---
+++ expensive/src/com/adarshr/expensive/AddActivity.java
@@ -0,0 +1,15 @@
+
+package com.adarshr.expensive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add);
+    }
+}
---
+++ expensive/src/com/adarshr/expensive/ExpensiveTabWidget.java
@@ -0,0 +1,39 @@
+
+package com.adarshr.expensive;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class ExpensiveTabWidget extends TabActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost(); // The activity TabHost
+        TabHost.TabSpec spec; // Resusable TabSpec for each tab
+        Intent intent; // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, AddActivity.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("add").setIndicator("Add",
+                res.getDrawable(R.drawable.ic_tab_add))
+                .setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, HistoryActivity.class);
+        spec = tabHost.newTabSpec("history").setIndicator("History",
+                res.getDrawable(R.drawable.ic_tab_history))
+                .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(0);
+    }
+}
